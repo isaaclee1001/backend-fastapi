3b6abc57bf9f099950faa553a41633969ef80ff1
leebobtheblob <leebobtheblob@naver.com>
2023-07-18 06:02:55 +0900

    added postgres db url

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:passw0rd@192.168.1.10/fastapi"
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # only for SQLite
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
