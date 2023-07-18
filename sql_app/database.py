81d2ea79da2daf445f97d3fa7cdb5fb1a17284f8
isaaclee1001 <isaaclee1001@atg.co.kr>
2023-07-18 06:14:16 +0000

    :wrench: postgresql connection required, change db url , remove check_same thred argument

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,14 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:passw0rd1!@192.168.1.100:15432/fastapi"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:passw0rd@192.168.1.10/fastapi"
 
 #actual database session
 #session local(db session) vs session (sqlalchemy)
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # only for SQLite
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # only for SQLite
+    SQLALCHEMY_DATABASE_URL # for postgres
+
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
